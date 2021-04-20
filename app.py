67de3b66afa79b9d658a999decb384b30d38ab4d
Denis Khakhalkin <denisqsound@gmail.com>
2021-04-20 11:13:52 +0300

    Fix output

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ def do_search() -> str:
     phrase_lower = phrase.lower()
     letters = request.form['letters']
     result = str(search4letters(phrase_lower, letters))
+    last_result = result.strip("{'}")
     title = 'Here are your results:'
-    return render_template('results.html', the_results=result, the_phrase=phrase, the_letters=letters, the_title=title)
+    return render_template('results.html', the_results=last_result, the_phrase=phrase, the_letters=letters, the_title=title)
 
 
 @app.route('/')
-@app.route('/entry')
+@app.route('/entry')    
 def entry_page() -> 'html':
     return render_template('entry.html', the_title="Welcome to search4letters on the Web")
 
